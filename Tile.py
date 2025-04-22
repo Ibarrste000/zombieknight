85b9d31e81c6ecbb19985f08c39e2a99dbe92af4
Stefano Ibarra <Ibarrste000@warren.k12.in.us>
2025-04-22 08:19:56 -0400

    initial commit

---
+++ Tile.py
@@ -0,0 +1,36 @@
+import pygame
+
+class Tile(pygame.sprite.Sprite):
+
+
+    def __init__(self, x, y, image_int, main_group, sub_group=""):
+        super().__init__()
+
+        if image_int == 1:
+            self.image = pygame.transform.scale(pygame.image.load(".assets/images/tiles/"))
+        elif image_int == 2:
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (2).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+            pass # TODO: remove this when done.
+        elif image_int == 3:
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (3).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+            pass # TODO: remove this when done.
+        elif image_int == 4:
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (4).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+            pass # TODO: remove this when done.
+        elif image_int == 5:
+            # TODO: just like image_int == 1.  store the transformed image in self.image.  This will use Tile (5).png
+            # TODO: 1 thing new.  add self to sub_group.  Just like you added self to main group down below.
+            pass # TODO: remove this when done.
+
+        #Add every tile to the main group
+        main_group.add(self)
+
+        #Get the rect of the image and position within the grid
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+        #Create a mask for better player collisions
+        self.mask = pygame.mask.from_surface(self.image)
